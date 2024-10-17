710254ec81186b429d674c01a570c4f64c3a085b
ondrej.kasparek <ondrej.kasparek@thermofisher.com>
2024-10-17 07:27:38 +0200

    Feedback Assignment 03.1

--- ToDoList/src/ToDoList.WebApi/Controllers/ToDoItemsController.cs
+++ ToDoList/src/ToDoList.WebApi/Controllers/ToDoItemsController.cs
@@ -28,7 +28,7 @@ public class ToDoItemsController : ControllerBase
 
     [HttpGet]
     public IActionResult Read()
-    { 
+    {
         try
         {
 
@@ -37,31 +37,66 @@ public class ToDoItemsController : ControllerBase
                 return NotFound();
             }
 
-            var ToDoItem = items.ToDoItemGetResponseDto.FromDomain.ToList();
+            //toto je spatne pochopene zadani (vlivem preklepu v nem pravdepodobne, v Tip ma byt misto funcke Add urcite Select) + mi to hlasi kompilacni error necekane :)
+            /*var ToDoItem = items.ToDoItemGetResponseDto.FromDomain.ToList();
             items.Add(ToDoItem);
-            return Ok(ToDoItem);
+            return Ok(ToDoItem);*/
+
+            /*
+            Pojdme si to udelat spolu :) Funkce read ma delat
+                - `200 OK` s listem všech úkolů ve formě DTO List<ToDoItemGetResponseDto>
+
+                co ten muj radek kodu dela
+                Select = metoda pro iterovatelne objekty (takze i List) co na zaklade toho objektu vytvori novy iterovatelny objekt
+
+                novy_objekt = puvodni_objekt.Select(operace co ma delat s kazdym objektem v puvodnim Listu ktery pak prida do puvodniho listu)
+
+                priklad:
+                List<int> cisla = new List<int>(){1,2,3} = list s inty ktery v sobe ma cisla 1,2,3
+                var novaCisla = cisla.Select(o => -1 * o) = nova cisla jsou vytvorena tak ze kazde cislo v listu cisla je vynasobeno -1 a pridano do noveho listu
+                takze novaCisla v sobe maji {-1,-2,-3}
+            */
+            var itemsToReturn = items.Select(ToDoItemGetResponseDto.FromDomain);
+            return Ok(itemsToReturn);
+
+            /*
+            BONUS
+            jde do udelat vsechno na jeden radek pomoci terarniho operatoru ?: ktery funguje podminka ? plati : neplati
+            takze to jde udelat jako
+
+            var itemsToReturn = items.Select(ToDoItemGetResponseDto.FromDomain);
+            return items == null ? NotFound() : Ok(itemsToReturn)
+
+            nebo to dokonce zkratit na
+            return items == null ? NotFound() : Ok(items.Select(ToDoItemGetResponseDto.FromDomain))
+            ale tohle je uz pro fajnsmekry :)
+            */
         }
-        catch(Exception ex)
+        catch (Exception ex)
         {
             return this.Problem(ex.Message, null, StatusCodes.Status500InternalServerError);
         }
-        
+
     }
 
     [HttpGet("{toDoItemId:int}")]
     public IActionResult ReadById(int toDoItemId, [FromBody] ToDoItemGetResponseDto request)
     {
+        //[FromBody] ToDoItemGetResponseDto request urcite nebudeme potrebovat -> chceme pouze Id ukolu co chceme precist
+
+        //prosim dodelat :)
         return Ok();
     }
 
     [HttpPut("{toDoItemId:int}")]
     public IActionResult UpdateById(int toDoItemId, [FromBody] ToDoItemUpdateRequestDto request)
     {
+        //prosim dodelat :)
         return Ok();
     }
 
     [HttpDelete("{toDoItemId:int}")]
-     public IActionResult DeleteById(int toDoItemId)
+    public IActionResult DeleteById(int toDoItemId)
     {
 
         try
